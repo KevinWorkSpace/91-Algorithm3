5646e13d4b6aeeecfc007ff0664d919d3c88e49a
KevinWorkSpace <11612431@mail.sustech.edu.cn>
2021-03-01 20:57:26 +0800

    finish day 11

---
+++ src/Basic/LinkedList/DetectCycle.java
@@ -0,0 +1,21 @@
+package Basic.LinkedList;
+
+public class DetectCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
+}
